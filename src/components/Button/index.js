0bec587ea1c70becc82ee847cf70d351d812e642
Rizky Samuel <rizky.samuel@ist.id>
2022-11-15 14:04:42 +0700

    Create Component Button

---
+++ src/components/Button/index.js
@@ -0,0 +1,116 @@
+// Button Component
+// --------------------------------------------------------
+
+import React from "react";
+import PropTypes from "prop-types";
+import ButtonMUI from "@mui/material/Button";
+import { CircularProgress } from "@mui/material";
+import Colors from "../../helpers/colors";
+
+const Button = ({
+  startIcon,
+  endIcon,
+  style,
+  type,
+  variant,
+  onClick,
+  disabled,
+  full,
+  children,
+  color,
+  sx,
+  isLoading,
+  rounded,
+  borderRadius,
+  padding,
+  ...others
+}) => {
+  return (
+    <ButtonMUI
+      type={type}
+      onClick={onClick}
+      variant={variant}
+      startIcon={startIcon}
+      endIcon={endIcon}
+      style={style}
+      disabled={disabled}
+      disableElevation
+      sx={{
+        display: "inline-block",
+        outline: 0,
+        textTransform: "unset",
+        width: full ? "100%" : "fit-content",
+        backgroundColor:
+          variant === "contained"
+            ? Colors.primary.hard
+            : color !== ""
+            ? color
+            : "",
+        padding: padding,
+        fontFamily: "Inter",
+        borderRadius:
+          borderRadius !== "" ? borderRadius : rounded ? "50px" : "10px",
+        ...sx,
+      }}
+      {...others}
+    >
+      {isLoading ? (
+        <CircularProgress
+          size="1rem"
+          color="secondary"
+          style={{ display: "flex" }}
+        />
+      ) : (
+        <p
+          style={{
+            margin: 0,
+            display: "flex",
+            gap: "4px",
+            justifyContent: "center",
+          }}
+        >
+          {children}
+        </p>
+      )}
+    </ButtonMUI>
+  );
+};
+
+Button.propTypes = {
+  style: PropTypes.object,
+  type: PropTypes.string,
+  variant: PropTypes.string,
+  tabIndex: PropTypes.number,
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  full: PropTypes.bool,
+  children: PropTypes.node,
+  startIcon: PropTypes.string,
+  endIcon: PropTypes.string,
+  color: PropTypes.string,
+  sx: PropTypes.object,
+  isLoading: PropTypes.bool,
+  rounded: PropTypes.bool,
+  borderRadius: PropTypes.string,
+  padding: PropTypes.string,
+};
+
+Button.defaultProps = {
+  style: {},
+  type: "button",
+  variant: "contained",
+  onClick: () => {},
+  disabled: false,
+  full: false,
+  children: "",
+  startIcon: "",
+  endIcon: "",
+  color: "",
+  sx: {},
+  isLoading: false,
+  rounded: false,
+  borderRadius: "",
+  padding: "13px 38px",
+};
+
+export default Button;
--- src/container/pages/DesignSystem/index.js
+++ src/container/pages/DesignSystem/index.js
@@ -4,8 +4,9 @@
 
 import React from "react";
 import PropTypes from "prop-types";
-import { Button } from "@mui/material";
 import Colors from "../../../helpers/colors";
+import Button from "../../../components/Button";
+import { ReactComponent as Eye } from "../../../assets/icon/Eye.svg";
 
 const DesignSystem = ({ propsName }) => {
   return (
@@ -19,9 +20,59 @@ const DesignSystem = ({ propsName }) => {
       }}
     >
       <div>
-        <h2>Button</h2>
+        <h2>Button Contained</h2>
         <Button variant="contained" style={{ textTransform: "unset" }}>
           Click Me!
+        </Button>
+      </div>
+      <div>
+        <h2>Button Outlined</h2>
+        <Button variant="outlined" style={{ textTransform: "unset" }}>
+          Click Me!
+        </Button>
+      </div>
+      <div>
+        <h2>Button Full</h2>
+        <Button variant="contained" full style={{ textTransform: "unset" }}>
+          Click Me!
+        </Button>
+      </div>
+      <div>
+        <h2>Button Loading</h2>
+        <Button
+          variant="contained"
+          isLoading
+          style={{ textTransform: "unset" }}
+        >
+          Click Me!
+        </Button>
+      </div>
+      <div>
+        <h2>Button Rounded</h2>
+        <Button variant="contained" rounded style={{ textTransform: "unset" }}>
+          Click Me!
+        </Button>
+      </div>
+      <div>
+        <h2>Button Custom Rounded</h2>
+        <Button
+          variant="contained"
+          borderRadius="5px"
+          style={{ textTransform: "unset" }}
+        >
+          Click Me!
+        </Button>
+      </div>
+      <div>
+        <h2>Button With Icon</h2>
+        <Button
+          variant="contained"
+          borderRadius="5px"
+          style={{ textTransform: "unset" }}
+          padding="0px 7px 0px 9px"
+        >
+          Detail
+          <Eye />
         </Button>
       </div>
     </div>
